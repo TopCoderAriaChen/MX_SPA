3d399b3a8933484537691ccd6dd09b4fadcbcbbc
Yujia Chen <97077025+AriaChen124@users.noreply.github.com>
2024-02-20 18:32:43 +0800

    Implement campus API endpoints
    Define campus-related API routes and create a CampusListApi resource to handle GET requests for listing campus entities.

---
+++ app/campus/controller.py
@@ -0,0 +1,14 @@
+
+from flask_restx import Namespace, Resource
+
+from app.campus.schema import CampusListSchema
+from .model import Campus
+
+
+api = Namespace("campus")
+
+@api.route("/")
+class CampusListApi(Resource): 
+    def get(self):
+        campus_list = list(Campus.objects())
+        return CampusListSchema.from_orm(campus_list)
