13303e99804ba4697ad03be50a5f787d1a966e94
Yujia Chen <97077025+AriaChen124@users.noreply.github.com>
2024-02-24 19:22:13 +0800

    Enhance login page styling and ensure Naive UI styles load last
    Updated LoginView.vue to include Naive UI components for a structured and styled login interface.
    Updated main.css for layout styling, and appended a meta tag in main.ts to guarantee Naive UI styles are prioritized last in the document head. By ensuring that the `meta` tag related to Naive UI is the last element in the `head`, Naive UI's styles are applied last, thus avoiding being overridden by any other styles defined earlier.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,14 +1,18 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import App from './App.vue';
+import router from './router';
 
-import App from './App.vue'
-import router from './router'
+import './assets/main.css';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+const meta = document.createElement('meta');
+meta.name = "naive-ui-style";
+document.head.appendChild(meta);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,7 +1,27 @@
 <script setup lang="ts">
+import { NSpace, NH1, NH5, NButton } from "naive-ui";
 </script>
 
 <template>
-    <div>Login</div>
-
+  <div class="login">
+    <n-space vertical class="login-card">
+      <n-h1>Welcome to Shangxue Online</n-h1>
+      <n-h5>Please login to continue</n-h5>
+      <n-button type="primary" tag="a">Hello</n-button>
+    </n-space>
+  </div>
 </template>
+
+<style lang="less">
+.login {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
+.login-card {
+  margin-right: 15vw;
+}
+</style>
