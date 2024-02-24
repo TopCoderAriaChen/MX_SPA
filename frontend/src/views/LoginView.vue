ccb920fffe74c9d32a49ee34c3e518c20ddcf679
Yujia Chen <97077025+AriaChen124@users.noreply.github.com>
2024-02-24 19:31:17 +0800

    Refine layout and styling for login view
    Enhanced the login view with Naive UI components, adding structured layout and primary button. Updated Tailwind configuration to include Vue files for styling.

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -4,7 +4,7 @@ import { NSpace, NH1, NH5, NButton } from "naive-ui";
 
 <template>
   <div class="login">
-    <n-space vertical class="login-card">
+    <n-space vertical class="login-card mr-10">
       <n-h1>Welcome to Shangxue Online</n-h1>
       <n-h5>Please login to continue</n-h5>
       <n-button type="primary" tag="a">Hello</n-button>
@@ -21,7 +21,7 @@ import { NSpace, NH1, NH5, NButton } from "naive-ui";
   align-items: center;
 }
 
-.login-card {
+.login-card{
   margin-right: 15vw;
 }
 </style>
