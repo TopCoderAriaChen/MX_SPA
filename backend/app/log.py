31bed21170caadfdb8c923da3b145b36f9323222
Yujia Chen <97077025+AriaChen124@users.noreply.github.com>
2024-02-28 08:58:52 +0800

    Enhance logging with request context information using correlation IDs and JSON in Flask
    Implemented a new logging format that enriches each log entry with request-specific details. The updates ensure comprehensive tracking of request attributes such as the HTTP method, request path, and query parameters. These are now logged alongside a unique correlation ID and timestamp for each request. This enhancement facilitates better monitoring, debugging, and correlation of log entries across the application lifecycle.

--- backend/app/log.py
+++ backend/app/log.py
@@ -1,8 +1,47 @@
+from datetime import datetime
 import logging
 import os
+import uuid
+import json
 
-from flask import Flask
+from flask import Flask, request
 from pythonjsonlogger import jsonlogger 
+
+class Correlation:
+    def __init__(self):
+        self.id = uuid.uuid4()
+        self.timestamp = datetime.utcnow()
+
+    def to_dict(self):
+        return{
+            "correlationId":str(self.id),
+            "correlationTimestamp":self.timestamp.isoformat(),
+        }
+
+class CorrelationLoggerDecorator:
+    def __init__(self, logger, correlation):
+        self.logger = logger
+        self.base = correlation
+
+    @staticmethod
+    def _decorate(base, extra):
+        return {**base, **extra}
+
+    def info(self, message, extra={}):
+        self.logger.info(message, extra=self._decorate(self.base, extra))
+
+    def warning(self, message, extra={}):
+        self.logger.warning(message, extra=self._decorate(self.base, extra))
+
+    def error(self, message, extra={}):
+        self.logger.error(message, extra=self._decorate(self.base, extra))
+
+    def debug(self, message, extra={}):
+        self.logger.debug(message, extra=self._decorate(self.base, extra))
+
+    def getChild(self, name):
+        return CorrelationLoggerDecorator(self.logger.getChild(name), self.base)
+
 
 def config_log(app: Flask):
     log_path = os.path.join(app.root_path, "logs")
@@ -16,4 +55,21 @@ def config_log(app: Flask):
     file_handler.setFormatter(json_formatter) 
     app.logger.addHandler(file_handler)
     app.logger.setLevel(logging.DEBUG)
+
+    # Config request logger
+    @app.before_request
+    def before_request():
+        request.correlation = Correlation()
+        request.logger = CorrelationLoggerDecorator(
+            app.logger, request.correlation.to_dict()
+        )
+        extra = {
+            "x-request-id": request.headers.get("x-request-id"),
+            "method": request.method,
+            "path": request.path,
+            "querys": json.dumps(request.args),
+        }
+        request.logger.info("request", extra)  
+    
+    
     return app
