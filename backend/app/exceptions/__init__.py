ea7a3a0dde4ba8862bcb2e922c67b0bd9945a6cd
Yujia Chen <97077025+AriaChen124@users.noreply.github.com>
2024-03-07 15:53:43 +0800

    Implement NoAuthorizationError handler for expired sessions

--- backend/app/exceptions/__init__.py
+++ backend/app/exceptions/__init__.py
@@ -1,10 +1,17 @@
 from flask import request
-from pydantic import ValidationError as PydanticValidationError
 from mongoengine import ValidationError as MongoengineValidationError
+from pydantic import ValidationError as PydanticValidationError
 from werkzeug.exceptions import HTTPException
+from flask_jwt_extended.exceptions import NoAuthorizationError
+
 from .permission_exceptions import PermissionDenied
 
 def register_resources_exception_handler(api):
+    @api.errorhandler(NoAuthorizationError)
+    def handle_authorization_error(e: NoAuthorizationError):
+        request.logger.error(getattr(e, "description", str(e)))
+        return {"code": 401, "message": getattr(e, "description", str(e))}, 401
+
     @api.errorhandler(PermissionDenied)
     def handle_permission_denied(e: PermissionDenied):
         request.logger.error(getattr(e, "description", str(e)))
@@ -36,3 +43,4 @@ def register_resources_exception_handler(api):
     def handle_default_exception(e: Exception):
         request.logger.error(str(e))
         return {"code": 500, "message": "Internal server error"}, 500
+        return {"code": 500, "message": "Internal server errror"}, 500
